389126dd3db2bc043332ba63942c55a4ebdd3d7d
Robin Chen <71460817+StellarNursery@users.noreply.github.com>
2020-10-14 09:17:40 -0700

    Add files via upload

---
+++ Hey_Day.sh
@@ -0,0 +1,11 @@
+#! /bin/bash/
+
+# prompt for user's name
+echo "First I'll need you to enter your name here for me: "
+
+# read in user input
+read name
+
+# print Hey message
+echo "Hey $name"
+echo "War Eagle!"
